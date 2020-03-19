66d8702a806b866e26001ec87259992f1c173046
DiegoColombo <diego.colombo89@gmail.com>
2020-03-19 09:20:42 -0300

    Blackjack

--- Blackjack/bj.py
+++ Blackjack/bj.py
@@ -1,6 +1,3 @@
-
-
-
 # import random
 
 # print('\t-'*20)
@@ -10,55 +7,85 @@
 # print('\t-'*20)
 # print('\n'*2)
 
-# jogadores = int(input('\nInforme a quantidade de jogadores: '))
+# jogadores = int(input('Informe a quantidade de jogadores 1 / 5 :\t'))
 
 
-# cartas = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+# dealer = [ 10, 2, 3, 4, 5, 7, 8, 9]
+# j1 = f"{dealer}" 
+# j2 = f"{dealer}"
+# j3 = f"{dealer}"
+# j4 = f"{dealer}"
+# j5 = f"{dealer}"
 
 
-# def embaralhar(cartas):
-#     lista_embaralha = []
-#     dealer = random.choice(cartas)
+# def blackjack(j1, j2, j3, j4, j5):
+#     lista = []
+#     for e in range(len(dealer)):
+#         if jogadores == 1:
+#             dealer = random.choice(dealer)
+#             soma_cartas = sum(dealer)
+#             lista.append(dealer)
+#             return lista
+#         print(f"Sua mão é de {soma_cartas} pontos, deseja continuar?\t SIM / NAO")
+            
+#         #if 'A' in dealer[0] and lista == []:
+#             #lista.append(11)
+#         #lista.remove(lista['A'])
+#             #continue
+#         #if dealer == 'A' or dealer == 'J' or dealer == 'Q' or dealer == 'K':
+#            # lista.append(10)
+#             #continue
+#         print('Deseja continuar?\t')
+#         sim = input("SIM\t NÃO\t")
+#         if sim == ('SIM'):
+#             continue
+#         if sim == ('NÃO'):
+#             print(lista)
+
+#     return lista
+
+
+
+# import random
+
+# A = 11
+# A1 = 1
+# J = 10
+# Q = 10
+# K = 10
+
+# dealer = (A, A1, 2, 3, 4, 5, 7, 8, 9, J, Q, K)
+
+
+# def choice(dealer):
+#     dealer = random.choice
+#     return dealer
     
-#     for dealer in cartas:
-#         print(dealer)
-#     return lista_embaralha
-    
-# print(embaralhar(dealer))
 
-import random
-
-print('\t-'*20)
-print('\n')
-cab ='''                                                                   BLACKJACK                                        \n'''    
-print(cab)
-print('\t-'*20)
-print('\n'*2)
+# print(choice(dealer))
 
 
-cartas  = ["A", "2", "3", "4","5", "6","7", "8", "9","10","J", "Q","K"]
-def embaralhar(cartas):
-    lista = []
-    for e in range(len(cartas)):
-        dealer = random.choice(cartas)
-        #print(dealer)
-        if 'A' in dealer[0] and lista == []:
-            lista.append(11)
-            #lista.remove(lista['A'])
-            continue
-        if dealer == 'A' or dealer == 'J' or dealer == 'Q' or dealer == 'K':
-            lista.append(10)
-            #lista.remove(lista['A'], lista['J'], lista['Q'], ista['K'])
-            continue
-     
-    return lista
+
+# print('\t-'*20)
+# print('\n')
+# cab ='''                                                                   BLACKJACK                                        \n'''    
+# print(cab)
+# print('\t-'*20)
+# print('\n'*2)
+
+# jogadores = int(input('Informe a quantidade de jogadores 1 / 5 :\t'))
 
 
-#print(embaralhar(cartas))
+#j1 = print(f"{dealer}")
 
-jogadores = int(input('Informe a quantidade de jogadores: \t'))
 
-vira_carta = (f"A sua carta virada foi:{embaralhar(cartas)} \t")
-somar_cartas = (f"A sua mão é de: {embaralhar(sum(cartas))} pontos\t")
-print('Deseja continuar?\t'*2)
-print('SIM\t NÃO\t')                             
+
+# Desculpe mas não consegui evoluir na lógica 
+
+
+
+
+
+
+
+
